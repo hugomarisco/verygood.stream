363032fc5d8cbadeadf27d141ea982e2eb5e36ff
Hugo Mendes <hello@hugomarisco.in>
2019-08-31 15:41:29 +0100

    wip

--- packages/commons/src/Logger.ts
+++ packages/commons/src/Logger.ts
@@ -3,7 +3,7 @@ import winston from "winston";
 export const Logger = winston.createLogger({
   format:
     process.env.NODE_ENV === "production"
-      ? winston.format.combine(winston.format.errors(), winston.format.json())
+      ? winston.format.json()
       : winston.format.combine(
           winston.format.timestamp(),
           winston.format.colorize(),
